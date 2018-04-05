801a4a687efcb110944343a300c29779bdb637d7
raghuveer <raghuveer.dendukuri@gmail.com>
2018-04-05 07:45:41 +0530

    added link to default page

--- public_html/templates/default-frontend/default-home.php
+++ public_html/templates/default-frontend/default-home.php
@@ -2,3 +2,4 @@
   defined('START') or die;
 ?>
 <h1>Welcome to <?php echo html_escaped_output($site_url_project_main); ?></h1>
+<p><strong>Github Page: </strong><a href="https://github.com/raghuveer/easeapp">Easeapp PHP Framework's Github Page</a></p>
